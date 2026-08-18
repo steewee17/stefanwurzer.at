83187a4456926d5d54d2fd7071615ceb74562a10
stefanwurzer <stefanwurzer.innos@gmail.com>
2026-08-18 11:06:14 +0200

    fix: simplify footer link to just Case Study

--- components.js
+++ components.js
@@ -84,7 +84,7 @@ class SiteFooter extends HTMLElement {
               <span class="fdot">·</span>
               <a href="${faqHref}">Häufige Fragen</a>
               <span class="fdot">·</span>
-              <a href="/ki-mitarbeiter/case-premium-leads/">Case Study (Premium-Leads)</a>
+              <a href="/ki-mitarbeiter/case-premium-leads/">Case Study</a>
               <span class="fdot">·</span>
               <a href="/kontakt">Kontakt</a>
             </div>
